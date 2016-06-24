f172d842129c1cd78fac85195ba3426e648ca35f
micayil <javadzzzr@gmail.com>
2016-06-24 17:57:20 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = { V_for_Vendetta , 135693512 },
+    sudo_users = { V_for_Vendetta , 180552880 },
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
